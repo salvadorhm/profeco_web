916187db73dfa272b57d6bd875ec263b74a2c626
SalvadorHM <salvadorhm@gmail.com>
2016-11-04 15:24:49 -0600

    Demo Profeco y CartoDB

---
+++ app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import web
+import json
+
+render = web.template.render('views/',cache=False)
+        
+urls = (
+    '/','index',
+    '/values(.*)', 'values',
+    '/about','about'
+)
+
+class index:
+    def GET(self):
+        return render.index()
+
+class values:        
+    def GET(self, datos):
+        datos=[]
+        with open('data/profeco.json','r') as file:
+            datos = json.load(file)
+        return render.values(datos['results'])
+
+class about:
+    def GET(self):
+        return render.about()
+
+if __name__ == "__main__":
+    app = web.application(urls, globals(), autoreload=False)
+    web.config.debug = True
+    app.run()
